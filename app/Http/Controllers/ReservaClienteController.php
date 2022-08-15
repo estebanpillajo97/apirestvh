e54ba8c3cd28b5c42cf68ad86d34b1f224888dcf
estebanpillajo97 <epatriciopm@hotmail.com>
2022-08-15 10:30:54 -0500

    actualizado

--- app/Http/Controllers/ReservaClienteController.php
+++ app/Http/Controllers/ReservaClienteController.php
@@ -21,6 +21,7 @@ class ReservaClienteController extends Controller
         ->join('reservaciones','reservaciones.res_id','=','reserva_clientes.res_id')
         ->join('tipo_cedulas','tipo_cedulas.tc_id','=','reserva_clientes.tc_id')
         ->join('num_personas_res','num_personas_res.np_id','=','reserva_clientes.np_id')
+        ->join('salones','salones.sa_id','=','reserva_clientes.sa_id')
         ->reorder('rc_fecha','desc')->get();
         echo(json_encode($datosReservaCliente));
     }
